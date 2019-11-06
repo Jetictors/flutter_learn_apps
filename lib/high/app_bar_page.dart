23e763b87a370f3722a8631c247ab67a03bf3fe5
Jetictors <zhengxcfutures@gmail.com>
2019-11-06 17:25:22 +0800

    1、新增appBar组件学习 2、新增Drawer组件学习

--- lib/high/app_bar_page.dart
+++ lib/high/app_bar_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 ///
@@ -12,6 +14,7 @@ class AppBarPage extends StatelessWidget{
   AppBarPage({Key key, this.title}) : super(key : key);
 
   final String title;
+  final _titleStyle = TextStyle(color: Colors.red);
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +24,140 @@ class AppBarPage extends StatelessWidget{
           centerTitle: true,
           title: Text(title),
         ),
-        body: Center(
-          child: Text(title),
+        body: Padding(
+          padding: EdgeInsets.only(left: 12, top: 12, right: 12),
+          child: ListView(
+            children: <Widget>[
+              _first(),
+              _second(),
+              _third(),
+              _fourth(),
+              _five(),
+              _six()
+            ],
+          )
         )
+    );
+  }
+
+  Widget _first(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("1、实现一个简易得AppBar", style: _titleStyle),
+        AppBar(
+          title: Text("测试一"),
+          backgroundColor: Colors.blue,
+        ),
+        Divider()
+      ],
+    );
+  }
+
+  Widget _second(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("2、标题居中，换左右按钮", style: _titleStyle),
+        AppBar(
+          title: Text("测试二"),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          leading: Builder(builder: (context){
+            return IconButton(icon: Icon(Icons.add, color: Colors.white), onPressed: (){
+              log("second示例中的左边按钮被点击了！");
+            });
+          })
+        ),
+        Divider()
+      ],
+    );
+  }
+
+  Widget _third(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("3、隐藏左边按钮和不需要左边按钮", style: _titleStyle),
+        AppBar(
+          title: Text("测试三"),
+          backgroundColor: Colors.blue,
+          leading: Icon(null)
+        ),
+        Divider(),
+        AppBar(
+          title: Text("测试三"),
+          backgroundColor: Colors.blue,
+          automaticallyImplyLeading: false
+        ),
+        Divider()
+      ],
+    );
+  }
+
+  Widget _fourth(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("4、添加阴影", style: _titleStyle),
+        AppBar(
+          title: Text("测试四"),
+          backgroundColor: Colors.blue,
+          // 阴影
+          elevation: 6,
+          // 外观
+          shape: OutlineInputBorder(),
+        ),
+        Divider()
+      ],
+    );
+  }
+
+  Widget _five(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("5、样式属性", style: _titleStyle),
+        AppBar(
+          title: Text("测试五"),
+          backgroundColor: Colors.blue,
+          elevation: 6,
+          // 左边按钮得样式 ，可以使用fallback()，size = 24， color = 黑色，opacity（透明度）= 1.0
+          iconTheme: IconThemeData(color: Colors.red, size: 36, opacity: 0.7),
+          // 文本样式
+          textTheme: TextTheme(title: TextStyle(color: Colors.red)),
+          // title与左边按钮的间距
+          titleSpacing: 24,
+        ),
+        Divider()
+      ],
+    );
+  }
+
+  Widget _six(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("6、样式属性", style: _titleStyle),
+        AppBar(
+          title: Text("测试六"),
+          backgroundColor: Colors.blue,
+          // actions 右边按钮
+          actions: <Widget>[
+            Builder(builder: (context){
+              return IconButton(icon: Icon(Icons.add, color: Colors.white), onPressed: (){
+                log("six示例中的右边第一个按钮被点击了！");
+              });
+            }),
+            Builder(builder: (context){
+              return FlatButton(child: Text("完成", style: TextStyle(color: Colors.white, fontSize: 16)), onPressed: (){
+                log("six示例中的右边第二个按钮被点击了！");
+              });
+            }),
+          ],
+        ),
+        Divider()
+      ],
     );
   }
 
--- lib/high/drawer_page.dart
+++ lib/high/drawer_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 ///
@@ -20,6 +22,71 @@ class DrawerPage extends StatelessWidget{
           backgroundColor: Colors.red,
           centerTitle: true,
           title: Text(title),
+          leading: Builder(builder: (context){
+            return IconButton(
+              icon: Icon(Icons.menu, color: Colors.white, size: 24),
+              onPressed: (){
+                // 打开侧边栏
+                Scaffold.of(context).openDrawer();
+                },
+            );
+          }),
+        ),
+        drawer: Drawer(
+          child: MediaQuery.removePadding(
+              context: context,
+              removeTop: true,
+              child: Container(
+                padding: EdgeInsets.only(top: 44),
+                color: Colors.blue[200],
+                child: ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset("imgs/ic_header_4.png", width: 72),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                        child: Text("Flutter", style: TextStyle(color: Colors.red, fontSize: 18))
+                      ),
+                      Text("我喜欢Flutter, 和我一起学习吧", textScaleFactor: 0.8),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 36, left: 12, right: 12),
+                          child: ListView(
+                            children: <Widget>[
+                              ListTile(
+                                title: Text("首页"),
+                                leading: Icon(Icons.home, color: Colors.red),
+                                onTap: (){ log("go home page"); },
+                              ),
+                              Divider(),
+                              ListTile(
+                                title: Text("设置"),
+                                leading: Icon(Icons.settings, color: Colors.red),
+                                onTap: (){ log("go Setting page"); },
+                              ),
+                              Divider(),
+                              ListTile(
+                                title: Text("关于我们"),
+                                leading: Icon(Icons.image, color: Colors.red),
+                                onTap: (){ log("go About page"); },
+                              ),
+                              Divider(),
+                              ListTile(
+                                title: Text("意见反馈"),
+                                leading: Icon(Icons.feedback, color: Colors.red),
+                                onTap: (){ log("go Feedback page"); },
+                              ),
+                              Divider()
+                            ],
+                          )
+                        )
+                      ),
+                    ],
+                  ),
+                )
+              )
+          )
         ),
         body: Center(
           child: Text(title),
